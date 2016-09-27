abdf9167d36191cf263246edf73b849a334c341d
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-09-27 16:57:15 +0100

    Making Docking stations get bike and release bike that works

---
+++ spec/bike_spec.rb
@@ -0,0 +1,9 @@
+require 'docking_station'
+
+describe Bike do
+  it { is_expected.to respond_to :working? }
+#  it 'is working?' do
+#     boris = Bike.new
+#     expect(boris.working?).to eq true
+#   end
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
 require 'docking_station'
+
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
-  #expect(DockingStation).to respond_to(:release_bike)
+  it 'release_bike' do
+    boris = Bike.new
+  end
+  it 'is working?' do
+    boris = Bike.new
+    expect(boris.working?).to eq true
+end
 end
