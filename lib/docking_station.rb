0fa324c9e0ee4d6822a19379ef92a9a8e94ab2a6
Caitlin <caitlin.gulliford@gmail.com>
2016-09-29 17:44:39 +0100

    Step 15, using private methods for exceptions

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,19 +4,28 @@ def initialize
   @bikes = []
 end
   def release_bike
-    fail 'No bike available' if @bikes.empty?
+    fail 'No bike available' if station_empty?
     @bikes.pop
   end
 
   def accept(bike)
-    fail 'Station is at capacity' if @bikes.length >= 20
+    fail 'Station is at capacity' if station_full?
     @bikes << bike
   end
-
 end
 
 class Bike
   def working?
     true
   end
+end
+
+private
+
+def station_full?
+  return true if @bikes.length >= 20
+end
+
+def station_empty?
+  return true if @bikes.empty?
 end
